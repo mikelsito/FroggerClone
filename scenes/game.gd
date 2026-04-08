782dc5d99b2e77acc0e638e45b7522c8330e2430
Michael Belsito <michaelbelsito@Michaels-MacBook-Air.local>
2026-04-08 12:54:52 -0500

    Added timer and title ui screen.

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,14 +1,19 @@
 extends Node2D
 
-var car_1: PackedScene = preload("res://scenes/car1.tscn")
+var car: PackedScene = preload("res://scenes/car1.tscn")
+var score: int = 0
 
 func go_to_title(body):
 	if body.name == "Player":
 		print("Ouch!")
 
 func _on_car_timer_timeout() -> void:
-	var yellow = car_1.instantiate() as Area2D
+	var yellow = car.instantiate() as Area2D
 	var start_pos = $CarStartPositions.get_children().pick_random() as Marker2D
 	yellow.position = start_pos.position
 	$Objects.add_child(yellow)
 	yellow.connect("body_entered", go_to_title)
+
+func _on_timer_timeout() -> void:
+	score += 1
+	$CanvasLayer/Label.text = str(score)
